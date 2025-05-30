9909a056b290ffcf6a38eb85330e2035e7cf3f7d
MrClouDD <sandhuajitesh@gmail.com>
2025-05-30 00:53:44 -0400

    Init

---
+++ leetcode/editor/en/no00002/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+package editor.en.no00002;
+
+import java.util.ArrayList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ArrayList<Integer> al1 = new ArrayList<>(), al2 = new ArrayList<>();
+        int num1 = 0, num2 = 0;
+        int count1 = 0, count2 = 0;
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
